441b038c97c6cce72099bee7e49441d016f4a4f3
Martin Konecny <100128860@student.burnley.ac.uk>
2024-05-24 15:50:02 +0100

    Index.php - improved code quality and comments

--- index.php
+++ index.php
@@ -1,75 +1,91 @@
 <?php
-include("_includes/config.inc");
-include("_includes/dbconnect.inc");
-include("_includes/functions.inc");
+    include("_includes/config.inc");
+    include("_includes/dbconnect.inc");
+    include("_includes/functions.inc");
 
-// Check if the user is trying to return after a failed login
-// if (isset($_GET['return']) && $_GET['return'] == "fail") {
-    //$data['message'] = "<h1>Login Failed. Please try again.</h1>";
-//}
+    // If user is logged in, display templates
+    if (isset($_SESSION['id'])) {
+        echo template("templates/partials/nav.php");
+        echo template("templates/default.php", $data);
 
-// If user is logged in, display admin page
-if (isset($_SESSION['id'])) {
-    $data['content'] = "
-    <div class='centered-image'>
-        <img src='indexheader.png' alt='Hero Image'>
-    </div>
-    <div class='centered-box' style='width: 80%;'>
-        <div class='box-left'>
-            <img src='indexpic.jpg' alt='Left Image' style='width: 100%;'>
-        </div>
-        <div class='box-right'>
-            <h1>WELCOME TO ADMIN PAGE</h1>
-            <h2>Please select an option from the navigation bar</h2>
-            <p>Nullam vel dapibus lacus. Sed nec justo id leo rutrum eleifend. Curabitur nec metus a libero ullamcorper tincidunt. Sed a aliquam nisi. Nullam dignissim eros vel commodo dapibus. Sed ac posuere ligula. Vivamus euismod nisl in sollicitudin vulputate. Integer tristique, dui ut blandit tempor, arcu velit faucibus lacus, nec fermentum nisi orci eu risus.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac tortor in velit tempor ullamcorper nec sed felis. Fusce eget nisi sed justo fringilla rhoncus. Mauris in sapien ut elit eleifend ultrices. Integer auctor odio eu neque facilisis, ut bibendum enim vehicula.</p>
-        </div>
-    </div>";
-    echo template("templates/partials/nav.php");
-    echo template("templates/default.php", $data);
-} else {
-    // If user is not logged in, display login page
-    echo template("templates/login.php", $data);
-}
-
-echo template("templates/partials/footer.php");
+    } else {
+        // If user is not logged in, display login page
+        echo template("templates/login.php", $data);
+    }
 ?>
 
+<div class='centered-image'>
+    <img src='indexheader.png' alt='Hero Image'>
+</div>
+<div class='centered-box' style='width: 80%;'>
+    <div class='box-left'>
+        <img src='indexpic.jpg' alt='Left Image' style='width: 100%;'>
+    </div>
+    <div class='box-right'>
+        <h1>WELCOME TO ADMIN PAGE</h1>
+        <h2>Please select an option from the navigation bar</h2>
+        <p>Nullam vel dapibus lacus. Sed nec justo id leo rutrum eleifend. Curabitur nec metus a libero ullamcorper tincidunt.
+            Sed a aliquam nisi. Nullam dignissim eros vel commodo dapibus. Sed ac posuere ligula. 
+            Vivamus euismod nisl in sollicitudin vulputate. Integer tristique, dui ut blandit tempor, 
+            arcu velit faucibus lacus, nec fermentum nisi orci eu risus.
+        </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+            Sed ac tortor in velit tempor ullamcorper nec sed felis. 
+        </p>
+    </div>
+</div>
+
+
 <style>
-.centered-image {
-    position: absolute;
-    top: 10%;
-    left: 44.78%;
-    transform: translate(-50%, -50%);
-}
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
 
-.centered-box {
-    position: absolute;
-    top: 70%;
-    left: 58%;
-    transform: translate(-50%, -50%);
-    display: flex;
-    justify-content: center;
-}
+    /* Font family */
+    body {
+        font-family: 'Roboto', sans-serif;
+    }
 
-.box-left {
-    flex: 1;
-}
 
-.box-right {
-    flex: 1;
-    padding-left: 20px;
-}
+    /* Positions the image in the center of the page */
+    .centered-image {
+        position: absolute;
+        top: 10%;
+        left: 44.78%;
+        transform: translate(-50%, -50%);
+    }
 
-.box-right h1 {
-    font-size: 40px;
-}
+    /* Centers the box horizontally and vertically */
+    .centered-box {
+        position: absolute;
+        top: 70%;
+        left: 58%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        justify-content: center;
+    }
 
-.box-right h2 {
-    font-size: 30px;
-}
+    /* Sets the width of the left box */
+    .box-left {
+        flex: 1;
+    }
 
-.box-right p {
-    font-size: 20px;
-}
+    /* Set the width and padding for the right box */
+    .box-right {
+        flex: 1;
+        padding-left: 20px;
+    }
+
+    /* Set the font size for h1 */
+    .box-right h1 {
+        font-size: 40px;
+    }
+
+    /* Set the font size for h2  */
+    .box-right h2 {
+        font-size: 30px;
+    }
+
+    /* Set the font size for p */
+    .box-right p {
+        font-size: 20px;
+    }
 </style>
